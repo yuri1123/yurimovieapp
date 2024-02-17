1f47c9dab21a869f88cd85145161b97f0b032c92
Choi Yuri <yurifrom1123@gmail.com>
2024-02-17 15:49:09 +0900

    inital commit

---
+++ android/app/src/main/kotlin/com/example/yurimovieapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yurimovieapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const YurisMovieApp());
+}
+
+class YurisMovieApp extends StatelessWidget {
+  const YurisMovieApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
